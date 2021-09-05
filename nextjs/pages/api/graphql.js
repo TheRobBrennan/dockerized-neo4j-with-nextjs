3d607fff3116376449172280db41c83deafaab96
Rob Brennan <rob@therobbrennan.com>
2021-09-04 18:32:52 -0700

    Initial implementation of our GraphQL API endpoint

--- nextjs/pages/api/graphql.js
+++ nextjs/pages/api/graphql.js
@@ -1,30 +1,48 @@
 import { gql, ApolloServer } from "apollo-server-micro"
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
+import { Neo4jGraphQL } from "@neo4j/graphql"
+import neo4j from "neo4j-driver"
+import "ts-tiny-invariant" // importing this module as a workaround for issue described here: https://github.com/vercel/vercel/discussions/5846
 
 const typeDefs = gql`
-  type User {
-    id: ID
+  # Custom GraphQL queries that will be handled by our custom resolvers can be defined here
+  type Query {
+    ping: Ping
   }
 
-  type Query {
-    getUser: User
+  type Ping {
+    message: String
+    timestamp: DateTime
+  }
+
+  type User {
+    username: String
+    created: DateTime @timestamp(operations: [CREATE])
   }
 `
 
 const resolvers = {
   Query: {
-    getUser: () => {
+    ping: () => {
       return {
-        id: "Foo",
+        message: "Pong",
+        timestamp: new Date(),
       }
     },
   },
 }
 
+const driver = neo4j.driver(
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+)
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, resolvers, driver })
+
 const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema: neoSchema.schema,
   playground: true,
+  introspection: true,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 })
 
