0782610e37c1c322824856e40649a331eec424da
Rob Brennan <rob@therobbrennan.com>
2021-09-04 22:05:18 -0700

    Use simple Tailwind styling for the main page

--- nextjs-with-tailwind/pages/index.tsx
+++ nextjs-with-tailwind/pages/index.tsx
@@ -2,44 +2,74 @@ import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
 
-import styles from "../styles/Home.module.css"
-
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>[DEMO] Neo4j and Next.js</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Explore the{" "}
-          <a href="https://neo4j.com" rel="noopener noreferrer" target="_blank">
-            Neo4j
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://nextjs.org"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Next.js
-          </a>{" "}
-          powered{" "}
-          <Link href="/api/graphql">
-            <a>GraphQL API</a>
-          </Link>
-        </h1>
+      <main>
+        <div className="bg-white">
+          <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+            <div className="text-center">
+              <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+                Explore the{" "}
+                <a
+                  href="https://neo4j.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Neo4j
+                </a>{" "}
+                and{" "}
+                <a
+                  href="https://nextjs.org"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Next.js
+                </a>{" "}
+                powered{" "}
+                <Link href="/api/graphql">
+                  <a target="_blank">GraphQL API</a>
+                </Link>
+              </p>
+              <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+                <a
+                  href="https://tailwindcss.com/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Tailwind CSS
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a href="https://vercel.com" target="_blank" rel="noopener noreferrer">
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+      <footer className="bg-white">
+        <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+          <p className="mt-8 text-center text-base text-gray-400">
+            <a
+              href="https://vercel.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered by{" "}
+              <span>
+                <Image
+                  src="/vercel.svg"
+                  alt="Vercel Logo"
+                  width={72}
+                  height={16}
+                />
+              </span>
+            </a>
+          </p>
+        </div>
       </footer>
     </div>
   )
