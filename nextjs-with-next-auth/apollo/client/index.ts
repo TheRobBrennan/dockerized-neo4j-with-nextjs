3f5f9c8a08da76796e13145cb3a6a4e3f26d6b95
Rob Brennan <rob@therobbrennan.com>
2021-12-11 14:49:51 -0800

    Use port 3002 for our Next-Auth app

--- nextjs-with-next-auth/apollo/client/index.ts
+++ nextjs-with-next-auth/apollo/client/index.ts
@@ -2,9 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   // It's ok for api url to be relative in browser.
-  uri: `${
-    typeof window === 'undefined' ? 'http://localhost:3000' : ''
-  }/api/graphql`,
+  uri: `${typeof window === 'undefined' ? 'http://localhost:3002' : ''
+    }/api/graphql`,
   cache: new InMemoryCache(),
 });
 
